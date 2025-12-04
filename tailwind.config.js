e21d922ab08530929a2706122bf71b0887b6547a
Sena <104799682+gabrsena@users.noreply.github.com>
2025-12-04 12:42:13 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Playfair Display"', 'serif'],
+        sans: ['"Inter"', 'sans-serif'],
+        poppins: ['"Poppins"', 'sans-serif'],
+        anton: ['"Anton"', 'sans-serif'],
+      },
+      colors: {
+        nobel: {
+          gold: '#B91C1C',
+          dark: '#1a1a1a',
+          cream: '#F9F8F4',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
